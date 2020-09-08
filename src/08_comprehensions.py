18f91ac4336a02f4dcb205f5e1dc87cd09b7d231
Regina Dircio <reginadircio@gmail.com>
2020-09-08 01:20:41 -0500

    Even list gives error :bug:

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -12,6 +12,9 @@ for more info regarding list comprehensions.
 
 y = []
 
+for x in range(5):
+    y.append(x)
+
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
@@ -19,23 +22,28 @@ print (y)
 
 y = []
 
+for item in range(0,10):
+    y.append(item**3)
+
 print(y)
 
 # Write a list comprehension to produce the uppercase version of all the
 # elements in array a. Hint: "foo".upper() is "FOO".
 
 a = ["foo", "bar", "baz"]
-
 y = []
+
+for item in a:
+    y.append(item.upper())
 
 print(y)
 
-# Use a list comprehension to create a list containing only the _even_ elements
-# the user entered into list x.
+# # Use a list comprehension to create a list containing only the _even_ elements
 
+# # the user entered into list x.
 x = input("Enter comma-separated numbers: ").split(',')
 
-# What do you need between the square brackets to make it work?
-y = []
+# # What do you need between the square brackets to make it work?
+y = [number for number in x if number % 2 == 0] # this erros out - not sure why
 
-print(y)
+print("Even numbers in the list: ", y)
